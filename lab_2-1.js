997e8a8e7c133c09e530ec4c509bc4d4992b3995
p23lrosenard <p23lrosenard@fairfieldprep.org>
2021-02-10 10:36:06 -0500

    Adding Labs 2-1 and 2-2

---
+++ lab_2-1.js
@@ -0,0 +1,11 @@
+// Auhor LR 2/10/2021
+
+let playerName = 'Leonard Rosenard';
+
+var firstName = playerName.slice(0,8)
+
+var lastName = playerName.slice(8,16)
+
+console.log (firstName);
+console.log (lastName);
+console.log (firstName + lastName);
---
+++ lab_2-2.js
@@ -0,0 +1,5 @@
+// Author LR 2/10/2021
+
+console.log(`Fairfield Prep`[4])
+console.log(`Fairfield Prep`.charAt(4))
+console.log(`Fairfield Prep`.slice(4,5))
